a0dca27ad8cba44f30c35940c55e3344f725ae1f
oxmc <67136658+oxmc@users.noreply.github.com>
2021-02-05 06:29:53 -0800

    Update Pi-AppsTerm_install.sh

--- files/Pi-AppsTerm_install.sh
+++ files/Pi-AppsTerm_install.sh
@@ -15,23 +15,22 @@ echo "${message}"
 echo "${message2}"
 bash ~/Pi-AppsTerm/files/checkpackages "git"
 if [ -d "/usr/local/bin/Pi-AppsTerm" ];then
-  echo "Pi-AppsTerm is already installed, but i will remove it to ensure it is the latest version."
-  rm -rf ~/Pi-AppsTerm
-  echo "Re-installing Pi-AppsTerm..."
-  wget -O - https://oxmc.github.io/Pi-Apps.info/files/Pi-AppsTerm_install.sh | bash || error "Unable to download Pi-AppsTerm!"
+  echo "Pi-AppsTerm is already installed! skipping..."
 else
   echo "Pi-AppsTerm is not installed! installing..."
   git clone https://github.com/oxmc/Pi-AppsTerm.git || error "Unable to download Pi-AppsTerm!"
+  bash ~/Pi-AppsTerm/files/Pi-AppsTerm_install.sh
 fi
 if [ -d "~/pi-apps" ];then
   echo "Pi-Apps is already installed! skipping..."
 else
   echo "Pi-Apps is not installed! Pi-AppsTerm requires this to be installed! installing..."
   git clone https://github.com/Botspot/pi-apps.git || error "Unable to download Pi-Apps!"
+  bash ~/pi-apps/install
 fi
-sudo mv Pi-AppsTerm /usr/local/bin/Pi-AppsTerm || error "Unable to move folder to /usr/local/bin!"
+sudo mv ~/Pi-AppsTerm /usr/local/bin/Pi-AppsTerm || error "Unable to move folder to /usr/local/bin!"
 cd /usr/local/bin/Pi-AppsTerm || error "Unable to change directiry to Pi-AppsTerm!"
 sudo chmod +x *.* || error "Unable to change executable permissons!"
-sudo cp /usr/local/bin/Pi-AppsTerm/files/Pi-AppsTerm /usr/local/bin/pi-apps || error "Unable to add pi-apps to terminal!"
-sudo chmod +x /usr/local/bin/pi-apps || error "Unable to change executable permissons! pi-apps will not work in terminal!"
-pi-apps version || error "Unable to run version command, pi-appsterm might be installed incorrectly!"
+sudo cp /usr/local/bin/Pi-AppsTerm/files/Pi-AppsTerm /usr/local/bin/pat || error "Unable to add pi-appsTerm to terminal!"
+sudo chmod +x /usr/local/bin/pat || error "Unable to change executable permissons! pi-appsTerm will not work in terminal!"
+pat version || error "Unable to run version command, pi-appsterm might be installed incorrectly!"
